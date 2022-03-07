660be2e74d437066073e87e2d222450cb0b96848
Woojin Son <75523802+ichbinderwooj@users.noreply.github.com>
2022-03-07 14:58:11 +0100

    Added firebase

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,12 @@
 export const environment = {
+  firebase: {
+    projectId: 'open-nation-simulation',
+    appId: '1:111044094341:web:19f117f2f0639d68da3cec',
+    storageBucket: 'open-nation-simulation.appspot.com',
+    apiKey: 'AIzaSyBDivBOY9zqU5SME0-u4GeuJjxF4ZWwd00',
+    authDomain: 'open-nation-simulation.firebaseapp.com',
+    messagingSenderId: '111044094341',
+    measurementId: 'G-EHG48GM23S',
+  },
   production: true
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,6 +3,15 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
+  firebase: {
+    projectId: 'open-nation-simulation',
+    appId: '1:111044094341:web:19f117f2f0639d68da3cec',
+    storageBucket: 'open-nation-simulation.appspot.com',
+    apiKey: 'AIzaSyBDivBOY9zqU5SME0-u4GeuJjxF4ZWwd00',
+    authDomain: 'open-nation-simulation.firebaseapp.com',
+    messagingSenderId: '111044094341',
+    measurementId: 'G-EHG48GM23S',
+  },
   production: false
 };
 
